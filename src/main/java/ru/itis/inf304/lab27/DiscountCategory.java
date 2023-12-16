e1f0e64304b3f665673d6bb5bd4ce4d2868d1f73
ToFik00 <tofikpetya@gmail.com>
2023-12-16 09:51:40 +0300

    add percents for DiscountCategory

--- src/main/java/ru/itis/inf304/lab27/DiscountCategory.java
+++ src/main/java/ru/itis/inf304/lab27/DiscountCategory.java
@@ -1,6 +1,15 @@
 package ru.itis.inf304.lab27;
 
 public enum DiscountCategory {
-    STANDARD(),
-    PENSIONER();
+    STANDARD(100),
+    PENSIONER(80);
+    final int percent;
+
+    DiscountCategory(int percent) {
+        this.percent = percent;
+    }
+
+    public int getPercent() {
+        return percent;
+    }
 }
