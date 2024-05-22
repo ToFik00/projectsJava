95141b3e66259ed3dbd2e633040f587f5f3679a9
ToFik <tofikpetya@gmail.com>
2024-05-22 03:04:31 +0300

    split the test into parts

--- src/test/java/ru/itis/inf304/SingletoneTest.java
+++ src/test/java/ru/itis/inf304/SingletoneTest.java
@@ -8,22 +8,26 @@ public class SingletoneTest {
 
     @Test
     public void singletonePatternTest() {
-        ThreadSingletoneTester thread1 = new ThreadSingletoneTester();
-        ThreadSingletoneTester thread2 = new ThreadSingletoneTester();
-        thread1.start();
-        thread2.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-
         Singletone singletone1 = Singletone.getInstance();
         Singletone singletone2 = Singletone.getInstance();
-        Assert.assertSame(thread1.getSingletone(), thread2.getSingletone());
         Assert.assertSame(singletone1, singletone2);
+    }
+
+    @Test
+    public void singletonePatternTestThreads() {
+        for (int i = 0; i < 100; i++) {
+            ThreadSingletoneTester thread1 = new ThreadSingletoneTester();
+            ThreadSingletoneTester thread2 = new ThreadSingletoneTester();
+            thread1.start();
+            thread2.start();
+
+            try {
+                thread1.join();
+                thread2.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Assert.assertSame(thread1.getSingletone(), thread2.getSingletone());
+        }
     }
 }
